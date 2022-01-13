d0d605e4258e306cd7f8d57560907118ed73573b
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-13 20:02:14 +0530

    Add project files.

---
+++ WebApplication23/Data/WebApplication23Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication23.Models;
+
+namespace WebApplication23.Data
+{
+    public class WebApplication23Context : DbContext
+    {
+        public WebApplication23Context (DbContextOptions<WebApplication23Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication23.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ WebApplication23/Migrations/20220113110233_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication23.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    PhoneNo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
